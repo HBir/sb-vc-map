5ff14b1d72017856668da41cf8e37a263185b63c
Hannes Birgersson <hannes@festinasweden.se>
2025-10-21 11:43:29 +0200

    fix build again

--- app/game-template/[filename]/route.ts
+++ app/game-template/[filename]/route.ts
@@ -2,8 +2,12 @@ import { NextResponse } from "next/server";
 import { promises as fs } from "fs";
 import path from "path";
 
-export async function GET(_req: Request, { params }: { params: { filename: string } }) {
-  const { filename } = params;
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const filename = url.pathname.split("/").pop();
+  if (!filename) {
+    return NextResponse.json({ error: "Bad request" }, { status: 400 });
+  }
   try {
     const filePath = path.join(process.cwd(), "app", "public", filename);
     const data = await fs.readFile(filePath);
