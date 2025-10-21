e1fd2123185f69128f513237937d92a1f44ce0f2
Hannes Birgersson <hannes@festinasweden.se>
2025-10-21 11:44:45 +0200

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -26,6 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <SpeedInsights/>
       <body className={`${geistSans.className} antialiased`}>
         <ThemeProvider
           attribute="class"
